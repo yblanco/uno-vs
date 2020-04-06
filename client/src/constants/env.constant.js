15f8cdf0a6f77bc438ce4a4a43529f5793627f7f
Yeinso Blanco <yeinsojbm@gmail.com>
2020-04-05 20:57:58 -0500

    dockerizing

--- client/src/constants/env.constant.js
+++ client/src/constants/env.constant.js
@@ -2,8 +2,8 @@ export const SERVER_PROTOCOL = process.env.REACT_APP_HOST_PROTOCOL || 'http';
 export const SERVER_HOST = process.env.REACT_APP_HOST_SERVER || 'localhost';
 export const SERVER_PORT = process.env.REACT_APP_PORT_SERVER || '3001';
 
-export const SERVER = `${SERVER_PROTOCOL}://${SERVER_HOST}:${SERVER_PORT}`;
-
-export const SECRET_REQUEST = 'mysecret';
+export const SECRET_REQUEST = process.env.REACT_APP_SECRET_REQUEST || 'mysecret';
 
 export const KEY_STORAGE = 'logged';
+
+export const SERVER = `${SERVER_PROTOCOL}://${SERVER_HOST}:${SERVER_PORT}`;
