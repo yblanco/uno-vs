75370aa67d383063c22b2cb7ce2bdeb04e7fbf87
Yeinso Blanco <yeinsojbm@gmail.com>
2020-04-08 21:56:31 -0500

    fix redirect on login

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -19,11 +19,9 @@ import { authUser, checkUser } from '../actions/auth.action';
 import { showSnackbarWarning } from '../actions/snackbar.action';
 
 export default translate('home')(({ t }) => {
-  const { state, dispatch } = useContext(Store);
   const history = useHistory();
   const location = useLocation();
-  const { from } = location.state || { from: { pathname: "/" } };
-
+  const { state, dispatch } = useContext(Store);
   const { app, auth } = state;
   const { ready, lang } = app;
   const { check, authenticated } = auth;
@@ -38,10 +36,11 @@ export default translate('home')(({ t }) => {
   }, [dispatch, ready, id])
 
   useEffect(() => {
-    if(authenticated !== false ) {
-      history.replace(from);
-    }
-  }, [dispatch, authenticated, from, history])
+    const { from } = location.state || { from: { pathname: "/index" } };
+      if(authenticated !== false) {
+        history.replace(from)
+      }
+  }, [dispatch, history, location, authenticated])
 
   return (
     <Content>
