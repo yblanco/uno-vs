20ed110161f57a0cddb7f15cff171dd1d6ddf9a1
Yeinso Blanco <yeinsojbm@gmail.com>
2020-04-11 03:17:13 -0500

    join game

--- client/src/pages/Play.js
+++ client/src/pages/Play.js
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react';
 import { Columns } from 'react-bulma-components';
 
+import { Redirect } from 'react-router';
+import routes from '../routes';
+
 import { Store } from '../reducers';
 
 export default () => {
@@ -8,8 +11,12 @@ export default () => {
   const { auth, game } = state;
   const { authenticated } = auth;
   const { id, name } = authenticated;
-  const { info, current } = game;
-  const { bet, cant, players } = info;
+  const { info, current = false } = game;
+  const { bet, cant, players = [] } = info;
+
+  if(current === false) {
+    return (<Redirect to={routes.getLink('new_game')} />);
+  }
 
   return (
     <Columns className='is-vcentered'>
